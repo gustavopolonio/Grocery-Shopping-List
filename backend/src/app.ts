e63c259373ba009e38cd67c569afaa93067756f4
gustavo_polonio <gustavopolonio1@gmail.com>
2025-08-09 20:26:38 -0300

    feat: config clerk express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
+import "dotenv/config";
 import express from "express";
+import { clerkMiddleware } from "@clerk/express";
 import { env } from "@/env";
 
 const app = express();
+
+app.use(clerkMiddleware());
 
 app.listen(env.PORT, () => {
   console.log(`App listening on port: ${env.PORT}`);
--- backend/src/env/index.ts
+++ backend/src/env/index.ts
@@ -4,7 +4,9 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.url(),
+  CLERK_PUBLISHABLE_KEY: z.string(),
+  CLERK_SECRET_KEY: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
