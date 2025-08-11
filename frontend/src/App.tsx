43b443627a6e9d9bcdb6d2baffc62f02690387af
gustavo_polonio <gustavopolonio1@gmail.com>
2025-08-11 14:54:13 -0300

    feat: Tanstack Query only retry query if not 401 status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ptBR, enUS } from "@clerk/localizations";
+import type { AxiosError } from "axios";
 import { Toaster } from "sonner";
 import { BrowserRouter, useLocation } from "react-router";
 import { localizedPath } from "@/utils";
 import { AppRoutes } from "@/AppRoutes";
 import "./lib/i18n";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error) => {
+        const axiosError = error as AxiosError;
+        return axiosError?.response?.status !== 401 && failureCount < 3;
+      },
+    },
+  },
+});
 
 function ClerkWithLang() {
   const location = useLocation();
